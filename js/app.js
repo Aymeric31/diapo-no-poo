41f0976a97f315cea83d51487fd2df3f5fa6212f
Aymeric <aymeric.gioia@hotmail.fr>
2017-01-22 16:21:38 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// Dans un fichier texte (JSON), décrivez une liste de photos ( titre et url), puis chargez le
+//  et utilisez le pour faire défiler automatiquement les photos dans une img.
+$(document).foundation()
+$.getJSON("index.json", function(info){
+
+	
+}
+
+function 
+// JSON.parse("image": "image1")
+
+	
+
